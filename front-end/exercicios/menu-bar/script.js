bb5dba88285a7328a7c50607cd76005ec1384d6a
Lucas Santana <109396382+lslucasO@users.noreply.github.com>
2023-11-16 11:37:26 -0300

    Add files via upload

---
+++ front-end/exercicios/menu-bar/script.js
@@ -0,0 +1,9 @@
+function appear(){
+    let menu_item = document.querySelector('.menu-item')
+    menu_item.style.width = '75%'
+}
+
+function disappear(){
+    let menu_item = document.querySelector('.menu-item')
+    menu_item.style.width = '0%'
+}
